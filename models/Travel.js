d94b173b317a070f191c51613f447db0cc3816bb
lauren <lauren@MacBook-Pro-de-user.local>
2020-09-11 15:22:37 +0200

    City model created

--- models/Travel.js
+++ models/Travel.js
@@ -8,27 +8,43 @@ const travelSchema = new mongoose.Schema({
     trim: true,
     maxlength: [50, 'Title can not be more than 50 characters'],
   },
-  cityStart: {
-    type: String,
-    required: [true, 'Please add the first city of your trip'],
-    trim: true,
-  },
-  cityEnd: {
-    type: String,
-    required: [true, 'Please add the last city of your trip'],
-    trim: true,
-  },
-  slug: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  cities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'City',
+    },
+  ],
+
+  // cityStart: {
+  //   type: String,
+  //   required: [true, 'Please add the first city of your trip'],
+  //   trim: true,
+  // },
+  // cityEnd: {
+  //   type: String,
+  //   required: [true, 'Please add the last city of your trip'],
+  //   trim: true,
+  // },
+  // slug: String,
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
 });
 
-travelSchema.pre('save', function (next) {
-  this.slug = slugify(this.title.toLowerCase());
-  console.log(`ici le slug: ${this.slug}`);
-  next();
-});
+travelSchema.path('cities').validate(function (travelSchema) {
+  if (!travelSchema) {
+    return false;
+  } else if (travelSchema.length < 2) {
+    return false;
+  }
+  return true;
+}, 'Travel needs to have at least two cities ');
+
+// travelSchema.pre('save', function (next) {
+//   this.slug = slugify(this.title.toLowerCase());
+//   console.log(`ici le slug: ${this.slug}`);
+//   next();
+// });
 
 module.exports = mongoose.model('Travel', travelSchema);
