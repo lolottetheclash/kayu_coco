edfbb7d71f7e50f2c472e7b3c36e531dfcd96a83
lauren <lauren@MacBook-Pro-de-user.local>
2020-09-23 09:23:39 +0200

    Add TravelsList & Item components

--- client/src/components/travels/components/TravelItem.js
+++ client/src/components/travels/components/TravelItem.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+
 const TravelItem = props => {
   const { travel } = props;
 
-  return <li>{travel.title}</li>;
+  return (
+    <Card>
+      <h2>Travel Item</h2>
+
+      <CardMedia
+        style={{ height: 0, paddingTop: '25%' }}
+        image="https://www.acs-ami.com/fr/blog/wp-content/uploads/2015/07/inspiration-voyage.jpg"
+        title={travel.title}
+      />
+      <CardContent>
+        <Typography> {travel.title}</Typography>
+      </CardContent>
+    </Card>
+  );
+  // <li>{travel.title}</li>;
 };
 export default TravelItem;
--- client/src/components/travels/components/TravelsList.js
+++ client/src/components/travels/components/TravelsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TravelItem from '../components/TravelItem';
+import { Grid } from '@material-ui/core';
 
 const TravelsList = props => {
   const { travels } = props;
@@ -9,11 +10,13 @@ const TravelsList = props => {
   return (
     <div>
       <h2>Travels List</h2>
-      <ul>
+      <Grid container spacing={3} style={{ padding: 24 }}>
         {props.travels.map(travel => (
-          <TravelItem key={travel._id} travel={travel} />
+          <Grid item xs={12} sm={6} lg={3} xl={2}>
+            <TravelItem key={travel._id} travel={travel} />
+          </Grid>
         ))}
-      </ul>
+      </Grid>
     </div>
   );
 };
